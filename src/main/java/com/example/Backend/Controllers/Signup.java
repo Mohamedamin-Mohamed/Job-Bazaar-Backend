25e65a65609c39a3363cef213c8bf7d54e08fc9f
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-05-30 09:24:49 -0500

    modified the response body

--- src/main/java/com/example/Backend/Controllers/Signup.java
+++ src/main/java/com/example/Backend/Controllers/Signup.java
@@ -7,20 +7,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 public class Signup {
+    private static final Logger LOGGER = Logger.getLogger(Signup.class.getName());
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/accounts/signup")
     public ResponseEntity<String> createUser(@RequestBody Request signupRequest){
+        LOGGER.info("Signup request received");
+
         String email = signupRequest.getEmail();
         String password = signupRequest.getPassword();
 
         boolean isUserCreated = userService.createUser(email, password);
         if(isUserCreated){
-            return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+            return new ResponseEntity<>("Account created successfully, Login to your account", HttpStatus.CREATED);
         }
         else{
             return new ResponseEntity<>("Account already exists", HttpStatus.CONFLICT);
