93416a5e02737861920a7d2cf7987538699a6d74
Mohamedamin Mohamed <MohamedMohamedamin@JohnDeere.com>
2024-06-12 21:16:28 -0500

    feat: new class to map UserDto request to a dynamodb item

--- src/main/java/com/example/Backend/Mappers/DynamoDbItemMapper.java
+++ src/main/java/com/example/Backend/Mappers/DynamoDbItemMapper.java
@@ -1,13 +1,14 @@
 package com.example.Backend.Mappers;
 
 import com.example.Backend.Dto.UserDto;
+import org.springframework.web.bind.annotation.Mapping;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserMapper {
-    public static Map<String, AttributeValue> toDynamoDbItemMap(UserDto user) {
+public class DynamoDbItemMapper {
+    public Map<String, AttributeValue> toDynamoDbItemMap(UserDto user) {
         Map<String, AttributeValue> item = new HashMap<>();
         item.put("email", AttributeValue.builder().s(user.getEmail()).build());
         item.put("hashedPassword", AttributeValue.builder().s(user.getHashedPassword()).build());
