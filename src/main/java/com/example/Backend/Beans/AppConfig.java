6d41b7235ffab9e9829faeabbf0bdad5671690aa
Mohamedamin Mohamed <MohamedMohamedamin@JohnDeere.com>
2024-06-12 21:22:10 -0500

    chore: added SecurityFilterChain bean

--- src/main/java/com/example/Backend/Beans/AppConfig.java
+++ src/main/java/com/example/Backend/Beans/AppConfig.java
@@ -6,8 +6,12 @@ import com.example.Backend.Services.UserService;
 import com.example.Backend.Utils.PasswordUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 
@@ -31,5 +35,19 @@ public class AppConfig {
     @Bean
     UserRepository userRepository(){
         return new UserRepository(dynamoDbClient(), dynamoDbItemMapper(), passwordutils());
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/**").permitAll();
+                    //auth.requestMatchers("/api/client/home").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(csrf -> {
+                    csrf.ignoringRequestMatchers("/**");
+                })
+                .formLogin(withDefaults())
+                .build();
     }
 }
