6ea0130fd92e666d197832a95f9f0bdccfc57cc3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-15 16:16:02 -0500

    feat: test class to test the functionalities of the UserService layer

--- src/test/java/com/JobBazaar/Backend/Services/UserServiceTest.java
+++ src/test/java/com/JobBazaar/Backend/Services/UserServiceTest.java
@@ -1,9 +1,9 @@
-package com.example.Backend.Services;
+package com.JobBazaar.Backend.Services;
 
-import com.example.Backend.Dto.RequestDto;
-import com.example.Backend.Dto.UserDto;
-import com.example.Backend.Repositories.UserRepository;
-import com.example.Backend.Utils.PasswordUtils;
+import com.JobBazaar.Backend.Dto.RequestDto;
+import com.JobBazaar.Backend.Dto.UserDto;
+import com.JobBazaar.Backend.Repositories.UserRepository;
+import com.JobBazaar.Backend.Utils.PasswordUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
