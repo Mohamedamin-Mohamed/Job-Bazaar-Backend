c5d6250fbae56da62e49534e2e41926328294eb7
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-15 16:12:40 -0500

    fix: changed the two methods to be non-static

--- src/main/java/com/JobBazaar/Backend/Utils/PasswordUtils.java
+++ src/main/java/com/JobBazaar/Backend/Utils/PasswordUtils.java
@@ -1,4 +1,4 @@
-package com.example.Backend.Utils;
+package com.JobBazaar.Backend.Utils;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
