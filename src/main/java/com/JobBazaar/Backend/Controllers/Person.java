a427d341cd8dda99564576c46b384f8628cf894e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-29 10:21:21 -0500

    feat: controller to return First and Last name of users

--- src/main/java/com/JobBazaar/Backend/Controllers/Person.java
+++ src/main/java/com/JobBazaar/Backend/Controllers/Person.java
@@ -1,6 +1,6 @@
 package com.JobBazaar.Backend.Controllers;
 
-import com.JobBazaar.Backend.Dto.Person;
+import com.JobBazaar.Backend.Dto.UserNames;
 import com.JobBazaar.Backend.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/api")
-public class User {
+public class Person {
+    private static final Logger LOGGER = Logger.getLogger(Person.class.getName());
     private UserService userService;
 
     @Autowired
-    public User (UserService userService) {
+    public Person(UserService userService) {
         this.userService = userService;
     }
     @GetMapping ("/person/{email}")
-    public ResponseEntity<Person> getPerson(@PathVariable String email) {
-        Person person = userService.getUsersInfo(email);
-        return ResponseEntity.ok(person);
+    public ResponseEntity<UserNames> getPerson(@PathVariable String email) {
+        LOGGER.info("Getting person with email: " + email);
+        UserNames names = userService.getUsersInfo(email);
+        if(names != null) {
+            LOGGER.info("Found person with email: " + names.getFirstName() + names.getLastName());
+            return ResponseEntity.ok(names);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
