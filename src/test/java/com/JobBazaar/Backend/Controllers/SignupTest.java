d35cddbce28c07c063eff7d204d7417b4546f32d
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-29 10:34:04 -0500

    feat: addition of first and last name fields

--- src/test/java/com/JobBazaar/Backend/Controllers/SignupTest.java
+++ src/test/java/com/JobBazaar/Backend/Controllers/SignupTest.java
@@ -1,4 +1,56 @@
+package com.JobBazaar.Backend.Controllers;
+import com.JobBazaar.Backend.Dto.SignupRequestDto;
+import com.JobBazaar.Backend.Services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 class SignupTest {
-  
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private Signup signup;
+
+    private SignupRequestDto requestDto;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+
+        requestDto = new SignupRequestDto();
+        requestDto.setEmail("test@test.com");
+        requestDto.setPassword("password");
+        requestDto.setFirstName("test");
+        requestDto.setLastName("com");
+    }
+
+    @Test
+    @DisplayName("Test to check if the users account has been created successfully")
+    void testCreateUser_True() {
+        when(userService.createUser(any(SignupRequestDto.class))).thenReturn(true);
+
+        ResponseEntity<String> response = signup.createUser(requestDto);
+
+        assertEquals(201, response.getStatusCode().value());
+        verify(userService, times(1)).createUser(any(SignupRequestDto.class));
+    }
+
+    @Test
+    @DisplayName("Test to check if the users account hasn't been created")
+    void testCreateUser_False(){
+        when(userService.createUser(any(SignupRequestDto.class))).thenReturn(false);
+
+        ResponseEntity<String> response = signup.createUser(requestDto);
+
+        assertEquals(409, response.getStatusCode().value());
+        verify(userService, times(1)).createUser(any(SignupRequestDto.class));
+    }
 }
