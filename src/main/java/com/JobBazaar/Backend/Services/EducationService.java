3030dda2b7bb45f4c645cd03439a22e2a6255d5e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-19 18:50:45 -0500

    feat: new servcice clas that interacts with EducationRepository

--- src/main/java/com/JobBazaar/Backend/Services/EducationService.java
+++ src/main/java/com/JobBazaar/Backend/Services/EducationService.java
@@ -1,2 +1,33 @@
-package com.JobBazaar.Backend.Services;public class EducationService {
+package com.JobBazaar.Backend.Services;
+
+import com.JobBazaar.Backend.Dto.EducationDto;
+import com.JobBazaar.Backend.Repositories.EducationRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.ParseException;
+
+public class EducationService {
+    private EducationRepository educationRepository;
+
+    @Autowired
+    public EducationService(EducationRepository educationRepository) {
+        this.educationRepository = educationRepository;
+    }
+
+    public boolean saveEducation(EducationDto educationDto) throws ParseException, JsonProcessingException {
+        return educationRepository.saveEducation(educationDto);
+    }
+
+    public boolean updateEducation(EducationDto educationDto) throws ParseException, JsonProcessingException {
+        return educationRepository.updateEducation(educationDto);
+    }
+
+    public boolean deleteEducation(String email) {
+        return educationRepository.deleteEducation(email);
+    }
+
+    public EducationDto getEducation(String email) {
+        return educationRepository.getEducation(email);
+    }
 }
