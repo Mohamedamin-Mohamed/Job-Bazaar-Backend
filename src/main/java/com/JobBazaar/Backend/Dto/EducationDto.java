289ab681eba79dd2ca3f4ad4f7f2a57ffc694112
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-19 18:46:16 -0500

    feat: new class that maps to education related request

--- src/main/java/com/JobBazaar/Backend/Dto/EducationDto.java
+++ src/main/java/com/JobBazaar/Backend/Dto/EducationDto.java
@@ -1,2 +1,67 @@
-package com.JobBazaar.Backend.Dto;public class EducationDto {
+package com.JobBazaar.Backend.Dto;
+
+public class EducationDto {
+    private String email;
+    private String school;
+    private String major;
+    private String degree;
+    private String description;
+    private String startDate;
+    private String endDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
 }
