dc932b45072c107444b797b5e4f0a6dda54cdcb6
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-28 10:15:13 -0500

    feat: new class that returns info of the user apart from the hashed password

--- src/main/java/com/JobBazaar/Backend/Dto/UserDto.java
+++ src/main/java/com/JobBazaar/Backend/Dto/UserDto.java
@@ -1,2 +1,50 @@
-package com.JobBazaar.Backend.Dto;public class UserDto {
+package com.JobBazaar.Backend.Dto;
+
+
+public class UserDto {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String createdAt;
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
