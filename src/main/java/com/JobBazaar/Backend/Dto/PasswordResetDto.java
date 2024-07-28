06ccf8b96e321631e347367da106b3dcecb64717
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-28 10:12:25 -0500

    feat: new class that maps to the Password reset json request

--- src/main/java/com/JobBazaar/Backend/Dto/PasswordResetDto.java
+++ src/main/java/com/JobBazaar/Backend/Dto/PasswordResetDto.java
@@ -1,2 +1,58 @@
-package com.JobBazaar.Backend.Dto;public class PasswordResetDto {
+package com.JobBazaar.Backend.Dto;
+
+public class PasswordResetDto {
+    private String email;
+    private String firstName;
+    private String password;
+    private String lastName;
+    private String role;
+    private String createdAt;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
