4f99f64e2a22d823dccdfdcfa2a182613fea10a3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-01 08:28:25 -0500

    feat: new controller that listens for requests related to jobs

--- src/main/java/com/JobBazaar/Backend/Controllers/Jobs.java
+++ src/main/java/com/JobBazaar/Backend/Controllers/Jobs.java
@@ -1,2 +1,65 @@
-package com.JobBazaar.Backend.Controllers;public class Jobs {
+package com.JobBazaar.Backend.Controllers;
+
+import com.JobBazaar.Backend.Dto.JobPostRequest;
+import com.JobBazaar.Backend.Services.JobService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class Jobs {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(Jobs.class);
+
+    private final JobService jobService;
+
+    @Autowired
+    public Jobs(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createJob(@RequestBody JobPostRequest jobPostRequest) {
+        LOGGER.info("Create job request received");
+
+        boolean jobCreated = jobService.createJob(jobPostRequest);
+
+        if (jobCreated) {
+            return new ResponseEntity<>("Job created successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Couldn't create job", HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/employer/{employerEmail}")
+    public ResponseEntity<?> getUploadedJobsByEmployerEmail(@PathVariable String employerEmail){
+        LOGGER.info("Received request to get uploaded jobs by employer with email {}", employerEmail);
+        List<Map<String, String>> jobsByEmployer = new ArrayList<>();
+
+        List<Map<String, AttributeValue>> jobsMap = jobService.getJobsByEmployerEmail(employerEmail);
+
+        if (jobsMap != null && !jobsMap.isEmpty()) {
+            for (Map<String, AttributeValue> jobAttributes : jobsMap) {
+                Map<String, String> jobMap = new HashMap<>();
+                for (Map.Entry<String, AttributeValue> entry : jobAttributes.entrySet()) {
+                    jobMap.put(entry.getKey(), entry.getValue().s());
+                }
+                jobsByEmployer.add(jobMap);
+        }
+            return ResponseEntity.ok(jobsByEmployer);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 }
