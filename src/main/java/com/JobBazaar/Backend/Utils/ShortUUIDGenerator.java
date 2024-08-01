4a10e9023c37c4d03512709a69e2c6f1c89b8b5a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-01 08:33:42 -0500

    feat: new class that generated uuids to be mapped to a unique jobId

--- src/main/java/com/JobBazaar/Backend/Utils/ShortUUIDGenerator.java
+++ src/main/java/com/JobBazaar/Backend/Utils/ShortUUIDGenerator.java
@@ -1,2 +1,13 @@
-package com.JobBazaar.Backend.Utils;public class ShortUUIDGenerator {
+package com.JobBazaar.Backend.Utils;
+
+import java.time.Year;
+import java.util.UUID;
+
+public class ShortUUIDGenerator {
+    public String generateShortUUID(){
+        String currentYear = String.valueOf(Year.now().getValue());
+        String baseUUID = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 8);
+
+        return currentYear + baseUUID;
+    }
 }
