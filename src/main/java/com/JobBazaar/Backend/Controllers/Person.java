2870317c805397edffff330a221eaf9185a1f382
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-01 08:29:32 -0500

    refractor: removed unused imports

--- src/main/java/com/JobBazaar/Backend/Controllers/Person.java
+++ src/main/java/com/JobBazaar/Backend/Controllers/Person.java
@@ -2,6 +2,8 @@ package com.JobBazaar.Backend.Controllers;
 
 import com.JobBazaar.Backend.Dto.UserDto;
 import com.JobBazaar.Backend.Services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Logger;
-
 @RestController
 @RequestMapping("/api/person")
 public class Person {
-    private static final Logger LOGGER = Logger.getLogger(Person.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(Login.class);
     private final UserService userService;
 
     @Autowired
@@ -22,11 +22,11 @@ public class Person {
         this.userService = userService;
     }
 
-    @GetMapping ("/{email}/")
+    @GetMapping("/{email}/")
     public ResponseEntity<UserDto> getPerson(@PathVariable String email) {
         LOGGER.info("Getting person with email: " + email);
         UserDto names = userService.getUsersInfo(email);
-        if(names != null) {
+        if (names != null) {
             LOGGER.info("Found: " + names.getFirstName() + names.getLastName());
             return ResponseEntity.ok(names);
         }
