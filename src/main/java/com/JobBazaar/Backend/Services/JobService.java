4283620e80586a1dacf68ba5962e4d93042c23f7
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-01 08:33:10 -0500

    feat: new service class that handles business logic of job related

--- src/main/java/com/JobBazaar/Backend/Services/JobService.java
+++ src/main/java/com/JobBazaar/Backend/Services/JobService.java
@@ -1,2 +1,35 @@
-package com.JobBazaar.Backend.Services;public class Job {
+package com.JobBazaar.Backend.Services;
+
+import com.JobBazaar.Backend.Dto.JobPostRequest;
+import com.JobBazaar.Backend.Repositories.JobRepository;
+import com.JobBazaar.Backend.Utils.ShortUUIDGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class JobService {
+
+    private final JobRepository jobRepository;
+    private final ShortUUIDGenerator shortUUIDGenerator;
+
+    @Autowired
+    public JobService(JobRepository jobRepository, ShortUUIDGenerator shortUUIDGenerator){
+        this.jobRepository = jobRepository;
+        this.shortUUIDGenerator = shortUUIDGenerator;
+    }
+
+    public boolean createJob(JobPostRequest jobPostRequest){
+        String jobId = shortUUIDGenerator.generateShortUUID();
+        jobPostRequest.setJobId(jobId);
+        return jobRepository.saveJob(jobPostRequest);
+    }
+
+    public List<Map<String, AttributeValue>> getJobsByEmployerEmail(String employerEmail){
+        return jobRepository.getJobsByEmployerEmail(employerEmail);
+    }
 }
