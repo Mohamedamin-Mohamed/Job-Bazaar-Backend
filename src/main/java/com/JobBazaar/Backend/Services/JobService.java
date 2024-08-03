de0af53550d1cc32276c1f3a5e983f8d1e4dce7a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-03 15:18:05 -0500

    feat: added try catch block to check if the incoming request has job id set and if it does, don't create a new job id for the incoming request

--- src/main/java/com/JobBazaar/Backend/Services/JobService.java
+++ src/main/java/com/JobBazaar/Backend/Services/JobService.java
@@ -1,41 +1,49 @@
 package com.JobBazaar.Backend.Services;
 
+import com.JobBazaar.Backend.Controllers.Login;
 import com.JobBazaar.Backend.Dto.JobPostRequest;
 import com.JobBazaar.Backend.Repositories.JobRepository;
 import com.JobBazaar.Backend.Utils.ShortUUIDGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class JobService {
-
+    private static Logger LOGGER = LoggerFactory.getLogger(Login.class);
     private final JobRepository jobRepository;
     private final ShortUUIDGenerator shortUUIDGenerator;
 
     @Autowired
-    public JobService(JobRepository jobRepository, ShortUUIDGenerator shortUUIDGenerator){
+    public JobService(JobRepository jobRepository, ShortUUIDGenerator shortUUIDGenerator) {
         this.jobRepository = jobRepository;
         this.shortUUIDGenerator = shortUUIDGenerator;
     }
 
-    public boolean createJob(JobPostRequest jobPostRequest){
-        if(jobPostRequest.getJobId().isEmpty()) {
-            String jobId = shortUUIDGenerator.generateShortUUID();
-            jobPostRequest.setJobId(jobId);
+    public boolean createJob(JobPostRequest jobPostRequest) {
+        System.out.println("Job id " + jobPostRequest.getJobId());
+        try {
+            if (jobPostRequest.getJobId() == null) {
+                String jobId = shortUUIDGenerator.generateShortUUID();
+                jobPostRequest.setJobId(jobId);
+            }
+            return jobRepository.saveJob(jobPostRequest);
+        } catch (Exception exp) {
+            LOGGER.error("Couldn't create job" + exp);
+            return false;
         }
-        return jobRepository.saveJob(jobPostRequest);
     }
 
-    public List<Map<String, AttributeValue>> getJobsByEmployerEmail(String employerEmail){
+    public List<Map<String, AttributeValue>> getJobsByEmployerEmail(String employerEmail) {
         return jobRepository.getJobsByEmployerEmail(employerEmail);
     }
 
-    public Map<String, AttributeValue> getJobsById(String employerEmail, String jobId){
+    public Map<String, AttributeValue> getJobsById(String employerEmail, String jobId) {
         return jobRepository.getJobsById(employerEmail, jobId);
     }
 }
