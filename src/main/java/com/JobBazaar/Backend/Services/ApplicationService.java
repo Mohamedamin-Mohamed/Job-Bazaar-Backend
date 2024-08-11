8f39bfa1fa6db266c00b47065e2f125e8aae67c3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-11 13:18:43 -0500

    feat: added new method that interacts with repository class to save new applications

--- src/main/java/com/JobBazaar/Backend/Services/ApplicationService.java
+++ src/main/java/com/JobBazaar/Backend/Services/ApplicationService.java
@@ -5,6 +5,7 @@ import com.JobBazaar.Backend.Repositories.ApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -18,5 +19,9 @@ public class ApplicationService {
 
     public boolean addApplication(ApplicationDto application, Map<String, Map<String, String>> fileUploadedToS3Info) {
         return applicationRepository.addApplication(application, fileUploadedToS3Info);
+    }
+
+    public List<Map<String, String>> getJobsAppliedTo(String applicantEmail){
+        return applicationRepository.getJobsAppliedTo(applicantEmail);
     }
 }
