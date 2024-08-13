e1b06b7cdc6cfa280eb2e1d12f18f77d99147112
Mohamed Khadhar <mohamedkhadhar@Mohameds-Air.AN-510-AP-I-AC>
2024-08-12 21:18:31 -0500

    refractor: removed unused imports and log statements

--- src/main/java/com/JobBazaar/Backend/Repositories/FilesUploadRepository.java
+++ src/main/java/com/JobBazaar/Backend/Repositories/FilesUploadRepository.java
@@ -5,12 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.enhanced.dynamodb.internal.AttributeValues;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,37 +23,36 @@ public class FilesUploadRepository {
         this.s3Client = s3Client;
     }
 
-    public Map<String, Map<String, String>> uploadFilesTos3(Map<String, byte[]> fileContents, List<String> fileNames){
+    public Map<String, Map<String, String>> uploadFilesTos3(Map<String, byte[]> fileContents, List<String> fileNames) {
         Map<String, Map<String, String>> uploadedFilesInfo = new HashMap<>();
 
         int i = 0;
-        for(String key : fileContents.keySet()){
+        for (String key : fileContents.keySet()) {
             byte[] fileContent = fileContents.get(key);
             String fileName = fileNames.get(i++);
-            if(fileContent.length != 0 && fileName != null && !fileName.isEmpty()){
+            if (fileContent.length != 0 && fileName != null && !fileName.isEmpty()) {
                 String keyName = "uploads/" + fileName;
                 String bucket = "userfilesuploads";
                 PutObjectResponse putObjectResponse = uploadFileTos3(bucket, keyName, fileContent);
-                if(putObjectResponse != null){
+                if (putObjectResponse != null) {
                     Map<String, String> fileInfo = new HashMap<>();
                     fileInfo.put("S3Key", keyName);
                     fileInfo.put("Etag", putObjectResponse.eTag());
                     LOGGER.info(fileInfo.toString());
                     uploadedFilesInfo.put(key, fileInfo);
-                }
-                else{
+                } else {
                     return new HashMap<>();
                 }
             }
         }
         return uploadedFilesInfo;
     }
-    public PutObjectResponse uploadFileTos3(String bucket, String keyName, byte[] fileContent){
+
+    public PutObjectResponse uploadFileTos3(String bucket, String keyName, byte[] fileContent) {
         PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(bucket).key(keyName).build();
-        try{
+        try {
             return s3Client.putObject(putObjectRequest, RequestBody.fromBytes(fileContent));
-        }
-        catch(Exception exp){
+        } catch (Exception exp) {
             LOGGER.error("Couldn't upload file to s3 bucket {}", exp.getMessage());
             return null;
         }
