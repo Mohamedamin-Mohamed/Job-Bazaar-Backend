ffc5185b670b8d337b01aec4ea46302c6db01966
Mohamed Khadhar <mohamedkhadhar@Mohameds-Air.AN-510-AP-I-AC>
2024-08-12 21:13:32 -0500

    fix: fixed merge conflicts

--- src/main/java/com/JobBazaar/Backend/Controllers/Jobs.java
+++ src/main/java/com/JobBazaar/Backend/Controllers/Jobs.java
@@ -13,72 +13,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.Map;
-    @RestController
-    @RequestMapping("/api/jobs")
-    public class Jobs {
-        private static final Logger LOGGER = LoggerFactory.getLogger(Login.class);
 
-        private final JobService jobService;
+@RestController
+@RequestMapping("/api/jobs")
+public class Jobs {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Login.class);
 
-        @Autowired
-        public Jobs(JobService jobService) {
-            this.jobService = jobService;
+    private final JobService jobService;
+
+    @Autowired
+    public Jobs(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> createJob(@RequestBody JobPostRequest jobPostRequest) {
+        LOGGER.info("Create job request received");
+
+        boolean jobCreated = jobService.createJob(jobPostRequest);
+
+        if (jobCreated) {
+            return new ResponseEntity<>("Job created successfully", HttpStatus.OK);
         }
 
-        @PostMapping("/")
-        public ResponseEntity<String> createJob(@RequestBody JobPostRequest jobPostRequest) {
-            LOGGER.info("Create job request received");
-
-            boolean jobCreated = jobService.createJob(jobPostRequest);
-
-            if (jobCreated) {
-                return new ResponseEntity<>("Job created successfully", HttpStatus.OK);
-            }
-            return new ResponseEntity<>("Couldn't create job", HttpStatus.BAD_REQUEST);
-        }
-      
         return new ResponseEntity<>("Couldn't create job", HttpStatus.BAD_REQUEST);
     }
-    @GetMapping("/")
-    public ResponseEntity<List<Map<String, String>>> getAvailableJobs(){
-        LOGGER.info("Received request to retrieve all available jobs");
-        List<Map<String, String>> availableJobs = new ArrayList<>();
 
-        List<Map<String, AttributeValue>> jobsMap = jobService.getAvailableJobs();
-        if(jobsMap != null && !jobsMap.isEmpty()){
-            for(Map<String, AttributeValue> jobsAttributes : jobsMap){
-                Map<String, String> jobMap = new HashMap<>();
-                for (Map.Entry<String, AttributeValue> entry : jobsAttributes.entrySet()) {
-                    jobMap.put(entry.getKey(), entry.getValue().s());
-                }
-                availableJobs.add(jobMap);
-            }
+    @GetMapping("/")
+    public ResponseEntity<List<Map<String, String>>> getAvailableJobs() {
+        LOGGER.info("Received request to retrieve all available jobs");
+
+        List<Map<String, String>> availableJobs = jobService.getAvailableJobs();
+
+        if (!availableJobs.isEmpty()) {
             return ResponseEntity.ok(availableJobs);
         }
-        return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.noContent().build();
     }
 
-        @GetMapping("/")
-        public ResponseEntity<List<Map<String, String>>> getAvailableJobs(){
-            LOGGER.info("Received request to retrieve all available jobs");
+    @GetMapping("/employer/{employerEmail}")
+    public ResponseEntity<List<Map<String, String>>> getUploadedJobsByEmployerEmail(@PathVariable String employerEmail) {
+        LOGGER.info("Received request to get uploaded jobs by employer with email {}", employerEmail);
+        List<Map<String, String>> jobsByEmployer = jobService.getJobsByEmployerEmail(employerEmail);
 
-            List<Map<String, String>> availableJobs = jobService.getAvailableJobs();
-
-            if(!availableJobs.isEmpty()) {
-                return ResponseEntity.ok(availableJobs);
-            }
-
-            return ResponseEntity.noContent().build();
-        }
-
-        @GetMapping("/employer/{employerEmail}")
-        public ResponseEntity<List<Map<String, String>>> getUploadedJobsByEmployerEmail(@PathVariable String employerEmail){
-            LOGGER.info("Received request to get uploaded jobs by employer with email {}", employerEmail);
-            List<Map<String, String>> jobsByEmployer = jobService.getJobsByEmployerEmail(employerEmail);
-
-            if(!jobsByEmployer.isEmpty()){
+        if (!jobsByEmployer.isEmpty()) {
 
             return ResponseEntity.ok(jobsByEmployer);
         }
@@ -86,12 +68,12 @@ import java.util.Map;
     }
 
     @GetMapping("/{employerEmail}/{jobId}")
-    public ResponseEntity<Map<String, String>> getJobsById(@PathVariable String employerEmail, @PathVariable String jobId){
+    public ResponseEntity<Map<String, String>> getJobsById(@PathVariable String employerEmail, @PathVariable String jobId) {
         LOGGER.info("Received request to fetch job by id: {}", jobId);
 
         Map<String, String> jobsMap = jobService.getJobsById(employerEmail, jobId);
 
-        if(!jobsMap.isEmpty()) {
+        if (!jobsMap.isEmpty()) {
             return ResponseEntity.ok(jobsMap);
         }
         return ResponseEntity.badRequest().build();
