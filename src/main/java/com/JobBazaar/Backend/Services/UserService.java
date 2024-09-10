b47aaa1aff806d1accecae7f3085a4aa6ddc8aac
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-09 20:23:38 -0500

    feat: added new methods that sends request to saveTopicArn and subscribing a user to a topic to the repository class

--- src/main/java/com/JobBazaar/Backend/Services/UserService.java
+++ src/main/java/com/JobBazaar/Backend/Services/UserService.java
@@ -1,24 +1,17 @@
 package com.JobBazaar.Backend.Services;
 
-import com.JobBazaar.Backend.Dto.AppUser;
-import com.JobBazaar.Backend.Dto.PasswordResetDto;
-import com.JobBazaar.Backend.Dto.RequestDto;
-import com.JobBazaar.Backend.Dto.SignupRequestDto;
-import com.JobBazaar.Backend.Dto.UserDto;
+import com.JobBazaar.Backend.Dto.*;
 import com.JobBazaar.Backend.Repositories.SnsRepository;
 import com.JobBazaar.Backend.Repositories.UserRepository;
 import com.JobBazaar.Backend.Utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
     private final UserRepository userRepository;
     private final SnsRepository snsRepository;
     private final PasswordUtils passwordUtils;
@@ -80,16 +73,7 @@ public class UserService implements UserDetailsService {
         snsRepository.saveTopicArn(topic, arn);
     }
 
-    public boolean subscriberAddedToTopic(SignupRequestDto signupRequest, String topicName) {
-        return snsRepository.addSubscriberToTopic(signupRequest, topicName);
-    }
-
-    public boolean sendWelcomeMessage(String sender, String recipient, String subject, String bodyHTML) {
-        return userRepository.sendWelcomeMessage(sender, recipient, subject, bodyHTML);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public void addSubscriberTopic(SignupRequestDto signupRequest, String topicName) {
+        snsRepository.addSubscriberToTopic(signupRequest, topicName);
     }
 }
