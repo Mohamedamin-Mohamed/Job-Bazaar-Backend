581a7f7e3858ff6edb466d36ba775ad9b50ce2e6
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-09 20:18:03 -0500

    feat: new service class that sends request to add and retrieve referrals to the repository class

--- src/main/java/com/JobBazaar/Backend/Services/ReferralsService.java
+++ src/main/java/com/JobBazaar/Backend/Services/ReferralsService.java
@@ -1,4 +1,31 @@
 package com.JobBazaar.Backend.Services;
 
+import com.JobBazaar.Backend.Dto.FeedbackDto;
+import com.JobBazaar.Backend.Dto.ReferralDto;
+import com.JobBazaar.Backend.Repositories.ReferralsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class ReferralsService {
+    private final ReferralsRepository referralsRepository;
+
+    @Autowired
+    public ReferralsService(ReferralsRepository referralsRepository) {
+        this.referralsRepository = referralsRepository;
+    }
+
+    public boolean addReferral(ReferralDto referralDto, Map<String, Map<String, String>> fileUploadedToS3Info) throws IOException {
+        return referralsRepository.addReferral(referralDto, fileUploadedToS3Info);
+    }
+
+    public List<Map<String, Object>> retrieveReferrals(String referrerEmail){
+        return referralsRepository.getReferrals(referrerEmail);
+    }
+
+    public List<Map<String, Object>> getAllReferrals(){
+        return referralsRepository.getAllReferrals();
+    }
 }
