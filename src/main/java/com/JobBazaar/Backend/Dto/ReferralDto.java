4264fb32770d42df1389ec20b7dbae52fa728e95
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-09 20:30:51 -0500

    feat: new dto class that maps to request to retrieve or add referrals

--- src/main/java/com/JobBazaar/Backend/Dto/ReferralDto.java
+++ src/main/java/com/JobBazaar/Backend/Dto/ReferralDto.java
@@ -1,4 +1,40 @@
 package com.JobBazaar.Backend.Dto;
 
 public class ReferralDto {
+    private String referrerEmail;
+    private String referrerName;
+    private String fileName;
+    private String createdAt;
+
+    public String getReferrerName() {
+        return referrerName;
+    }
+
+    public void setReferrerName(String referrerName) {
+        this.referrerName = referrerName;
+    }
+
+    public String getReferrerEmail() {
+        return referrerEmail;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setReferrerEmail(String referrerEmail) {
+        this.referrerEmail = referrerEmail;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
