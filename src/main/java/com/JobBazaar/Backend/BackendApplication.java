eec813107ca46f2c6a055cfeadbdf3eda073567f
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-09 20:35:39 -0500

    refractor: removed unused code

--- src/main/java/com/JobBazaar/Backend/BackendApplication.java
+++ src/main/java/com/JobBazaar/Backend/BackendApplication.java
@@ -2,14 +2,11 @@ package com.JobBazaar.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
