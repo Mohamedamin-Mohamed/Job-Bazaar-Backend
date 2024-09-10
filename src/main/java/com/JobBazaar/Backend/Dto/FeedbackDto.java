fa05b3231d1004be50a49c45df3aeb2f5d0456df
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-09 20:30:33 -0500

    feat: new dto class that maps to request to retrieve or add feedbacks

--- src/main/java/com/JobBazaar/Backend/Dto/FeedbackDto.java
+++ src/main/java/com/JobBazaar/Backend/Dto/FeedbackDto.java
@@ -1,4 +1,50 @@
 package com.JobBazaar.Backend.Dto;
 
 public class FeedbackDto {
+    private String applicantEmail;
+    private String jobId;
+    private String feedbackDate;
+    private String feedback;
+    private String status;
+
+    public String getFeedbackDate() {
+        return feedbackDate;
+    }
+
+    public void setFeedbackDate(String feedbackDate) {
+        this.feedbackDate = feedbackDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setApplicantEmail(String applicantEmail) {
+        this.applicantEmail = applicantEmail;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public String getApplicantEmail() {
+        return applicantEmail;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
