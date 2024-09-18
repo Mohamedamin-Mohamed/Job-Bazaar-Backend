7a60eeb7b478ebe36e0eba431986770b59a40b91
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-17 20:32:31 -0500

    feat: updated createUserExists by adding supresswarnings annotation

--- src/test/java/com/JobBazaar/Backend/Controllers/ReferralsTest.java
+++ src/test/java/com/JobBazaar/Backend/Controllers/ReferralsTest.java
@@ -1,4 +1,30 @@
+package com.JobBazaar.Backend.Controllers;
+
+import com.JobBazaar.Backend.Services.FilesUploadService;
+import com.JobBazaar.Backend.Services.ReferralsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class ReferralsTest {
-  
+    @Mock
+    ReferralsService referralsService;
+
+    @Mock
+    FilesUploadService filesUploadService;
+
+    @InjectMocks
+    Referrals referrals;
+
+    @Test
+    void addReferral_Successful() {
+
+    }
 }
