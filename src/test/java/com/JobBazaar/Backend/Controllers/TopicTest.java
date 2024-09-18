fd0db857b2cbb2e0658af66022671c1ea45f4ceb
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-17 20:33:47 -0500

    feat: added new test for creating a topic arn

--- src/test/java/com/JobBazaar/Backend/Controllers/TopicTest.java
+++ src/test/java/com/JobBazaar/Backend/Controllers/TopicTest.java
@@ -1,12 +1,11 @@
 package com.JobBazaar.Backend.Controllers;
 
-import com.JobBazaar.Backend.Services.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import com.JobBazaar.Backend.Services.SnsService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import static org.mockito.ArgumentMatchers.any;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.http.SdkHttpResponse;
 import software.amazon.awssdk.services.sns.SnsClient;
@@ -14,49 +13,50 @@ import software.amazon.awssdk.services.sns.model.CreateTopicRequest;
 import software.amazon.awssdk.services.sns.model.CreateTopicResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class TopicTest {
     @Mock
     SnsClient snsClient;
 
     @Mock
-    UserService userService;
+    SnsService snsService;
 
     @InjectMocks
     Topic topic;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
     void createTopicSuccessful() {
+        String topicName = "Notifications";
         SdkHttpResponse httpResponse = SdkHttpResponse.builder().statusCode(200).build();
+        CreateTopicResponse topicResponse = (CreateTopicResponse) CreateTopicResponse.builder().
+                topicArn("topic-arn").sdkHttpResponse(httpResponse).build();
 
-        CreateTopicResponse response = (CreateTopicResponse) CreateTopicResponse.builder().topicArn("topic-arn").
-                sdkHttpResponse(httpResponse).build();
+        when(snsClient.createTopic(any(CreateTopicRequest.class))).thenReturn(topicResponse);
+        doNothing().when(snsService).saveTopicArn(anyString(), anyString());
 
-        when(snsClient.createTopic(any(CreateTopicRequest.class))).thenReturn(response);
-        String result = topic.createTopic("topic-arn");
+        String result = topic.createTopic(topicName);
 
         assertNotNull(result);
-        assertTrue(result.contains("topic-arn"));
+        assertTrue(result.contains(topicResponse.topicArn()));
         verify(snsClient).createTopic(any(CreateTopicRequest.class));
     }
 
     @Test
-    void createTopicExceptionThrown() {
+    void createTopicFailed() {
+        String topicName = "Notifications";
         SdkHttpResponse httpResponse = SdkHttpResponse.builder().statusCode(500).build();
-        CreateTopicResponse response = (CreateTopicResponse) CreateTopicResponse.builder().topicArn("topic-arn").
-                sdkHttpResponse(httpResponse).build();
+        CreateTopicResponse topicResponse = (CreateTopicResponse) CreateTopicResponse.builder().
+                topicArn("topic-arn").sdkHttpResponse(httpResponse).build();
 
-        when(snsClient.createTopic(any(CreateTopicRequest.class))).thenReturn(response);
+        when(snsClient.createTopic(any(CreateTopicRequest.class))).thenReturn(topicResponse);
 
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> topic.createTopic("topic-arn"));
-        assertEquals(500, exception.getStatusCode().value());
-        assertEquals("Cannot create topic topic-arn", exception.getReason());
+        ResponseStatusException statusException = assertThrows(ResponseStatusException.class, ()-> topic.createTopic(topicName));
+
+        assertEquals(500, statusException.getStatusCode().value());
+        assertEquals("Cannot create topic Notifications", statusException.getReason());
     }
-
 }
