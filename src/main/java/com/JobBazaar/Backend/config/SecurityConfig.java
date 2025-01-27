f3b58f37ad5a685f3d8ca373bee74b2e8f3f4c1e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 18:32:39 -0600

    refractor: optimized imports

--- src/main/java/com/JobBazaar/Backend/config/SecurityConfig.java
+++ src/main/java/com/JobBazaar/Backend/config/SecurityConfig.java
@@ -1,26 +1,20 @@
 package com.JobBazaar.Backend.config;
 
-import com.JobBazaar.Backend.JwtToken.JwtTokenService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig implements WebMvcConfigurer {
-
-    private final JwtTokenService jwtTokenService;
-
-    public SecurityConfig(JwtTokenService jwtToken) {
-        this.jwtTokenService = jwtToken;
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")
-                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowCredentials(false);
     }
 }
