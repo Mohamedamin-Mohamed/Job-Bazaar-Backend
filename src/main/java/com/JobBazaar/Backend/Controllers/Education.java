2216be45b32ff875cc4c8d07eff4ac66880d88cb
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-01-26 18:34:18 -0600

    refractor: optimized imports

--- src/main/java/com/JobBazaar/Backend/Controllers/Education.java
+++ src/main/java/com/JobBazaar/Backend/Controllers/Education.java
@@ -6,14 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.util.logging.Logger;
@@ -42,6 +35,7 @@ public class Education {
     public ResponseEntity<EducationDto> getEducation(@PathVariable String email) {
         LOGGER.info("Received request to retrieve education");
         EducationDto educationDto = educationService.getEducation(email);
+
         if (educationDto != null) {
             return ResponseEntity.ok(educationDto);
         }
